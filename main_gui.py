2f7e8a7f5e2c4b3bea42370c1269accdd15704e3
no_username_cm <haruka.pjsk@gmail.com>
2025-08-13 01:13:22 +0700

    Update main_gui.py

--- main_gui.py
+++ main_gui.py
@@ -1,6 +1,5 @@
 import os
 import tkinter
-from cProfile import label
 from tkinter import *
 USER = os.getlogin()
 
@@ -10,18 +9,29 @@ main_menu.title("My computer")
 main_menu.iconphoto(False, tkinter.PhotoImage(file=r"C:\Users\ComServices\PycharmProjects\My computer\image file\testimage.png"))
 #why th do i spend sm time on icon 32x32 TO GET SOMETHING LIKE THIS
 
+touch_button = Button(main_menu)
+
+# its start text
 mypc_text = Label(main_menu, text="Hello, how may i can help you today?", font= 10) #make a text
+#enter to confirm the commands
 enter = Button(main_menu, text="Enter")
 
-mypc_face = PhotoImage(file=r"C:\Users\ComServices\PycharmProjects\My computer\image file\pcicon.png")
-textbox = Entry(main_menu, justify=LEFT, width=50)
-button_for_more = Button(main_menu, text="V", width=2, height=1)
+#face to display
+mypc_face = PhotoImage(file=r"C:\Users\ComServices\PycharmProjects\My computer\image file\my_pc_high_pizel.png")
+#this one is for reaction when touch it
+mypc_smileface = PhotoImage(file=r"C:\Users\ComServices\PycharmProjects\My computer\image file\my_pc_^-^.png")
 
-#mypc_image_on_main_menu
-mypc_text.pack()
-textbox.pack()
-button_for_more.pack()
-enter.pack()
+mypc_show_image = Label(main_menu, image=mypc_face, width=250, height=140)
+textbox = Entry(main_menu, justify=LEFT, width=50) # to choose or type the command in
+button_for_more = Button(main_menu, text="V", width=2, height=1) # look for more commands
+
+# mypc_image_on_main_menu
+mypc_text.place(x=60, y=10)
+mypc_show_image.place(x=68, y=45)
+textbox.place(x=50, y=191)
+#button_for_more.pack()
+#enter.pack()
+touch_button.pack()
 
 main_menu.resizable(False, False)
 main_menu.mainloop()
