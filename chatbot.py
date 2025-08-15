b7a44e837dd1119d79fa640584d70247ee1cab87
no_username_cm <haruka.pjsk@gmail.com>
2025-08-15 13:02:04 +0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,21 +1,28 @@
 import tkinter as tk
 from tkinter import *
 
-chat = Tk()
-chat.geometry("400x300")
-chat.title("Chat with me")
-sent = Button(chat, text="Send", width=10, height=3) #to send text to bot
-save = Button(chat, text="Save", width=6)
-load = Button(chat, text="Load", width=6)
-chat_textbox = Text(chat, height=4, width=46)
-chat.iconphoto(False, tk.PhotoImage(file=r"C:\Users\ComServices\PycharmProjects\My computer\image file\testimage.png"))
+chat = Tk() # create window
+chat.geometry("400x300") #change window size
+chat.title("Talk to me") 
+sent = Button(chat, text="Send", width=10, height=3) # a button to send text to bot
+save = Button(chat, text="Save", width=6) # save the conversation
+load = Button(chat, text="Load", width=6) # load the conversation
+chat_textbox = Text(chat, height=4, width=46) # input text
+chat.iconphoto(False, tk.PhotoImage(file=r"C:\Users\ComServices\PycharmProjects\My computer\image file\testimage.png")) # change icon 
 
 #set place for gui
-
 chat_textbox.place(x=0, y=245)
-
 load.place(x=50, y=0)
 save.place(x=0, y=0)
 sent.place(x=321, y=245)
 chat.resizable(False, False)
 chat.mainloop()
+
+def send_button():
+  # make a sent button
+
+def display_messange():
+  # displaying messange
+
+def scroll():
+  # make scroll
